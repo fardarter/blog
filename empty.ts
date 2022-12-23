04f05fec863c2c8236468b2c9e1abe91ae14410a
Saul Nachman <saulnachman@gmail.com>
2022-12-23 16:24:44 +0200

    feat: adds mdx and removes cypress

---
+++ empty.ts
@@ -0,0 +1,2 @@
+export {};
+// this file exists to make sure that the vscode does not add an error to the tsconfig.json file due to there being no ts files in the project. it's a dumb and annoying solution, but it works.
